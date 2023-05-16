7795c85d354bd9c11f49c184bbba3912f385a915
root <root@MezTop.localdomain>
2023-05-16 17:49:24 +1000

    added background script and API fetch

---
+++ background.js
@@ -0,0 +1,33 @@
+// Background script.js
+// This script runs in the background of the Chrome extension
+
+// Function to send an HTTP request
+function sendHttpRequest(data) {
+    fetch('http://envirocost.australiaeast.cloudapp.azure.com/structured', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        // Send the response back to the content script
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, responseData);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, { error: error.message });
+        });
+      });
+  }
+  
+  // Listen for messages from the content script
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // Call the function to send the HTTP request
+    sendHttpRequest(message);
+  });
+  
--- content.js
+++ content.js
@@ -1,61 +1,6 @@
-function addCustomDiv() {
-  // Get the body element of the page
-  const body = document.querySelector('body');
-
-  // Create a new div element
-  const div = document.createElement('div');
-
-  // Find the shopping cart element on the page
-  const cart = document.querySelector('.cart'); // Replace with the class name of the shopping cart element
-
-  // Extract the list of items from the cart element
-  const items = [];
-  const regex = /(?:<li>|<tr>).*?(?=<\/li>|<\/tr>)/gs;
-  const matches = cart.innerHTML.matchAll(regex);
-  for (const match of matches) {
-    const itemText = match[0].replace(/<(?:.|\n)*?>/gm, '').trim();
-    if (itemText !== '') {
-      items.push(itemText);
-    }
-  }
+// fetch('http://envirocost.australiaeast.cloudapp.azure.com/structured', {
 
 
-
-  // Display the list of items in the new div element
-  const listItems = items.map(item => `<li>${item}</li>`).join('');
-  div.innerHTML = `<h1>Shopping Cart Items:</h1><ul>${listItems}</ul>`;
-  console.log(listItems);
-  // Append the div to the body element
-  body.appendChild(div);
-}
-
-// Function to fetch data and send it to the API
-function fetchDataAndSendToAPI() {
-  // Read the specific ID or div on the website
-  const element = document.getElementById('your-id');
-  const data = {
-    req: [element.innerText, "Item 2"]
-  };
-
-  // Send JSON data to API
-  fetch('http://envirocost.australiaeast.cloudapp.azure.com/structured', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => response.json())
-    .then(responseData => {
-      // Process the response data
-      console.log(responseData);
-
-      // You can perform further actions here using the response data
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
 
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -65,6 +10,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-// Call the function when the content script is loaded or whenever necessary
-fetchDataAndSendToAPI();
+console.log("EnviroCost Ext Loaded")
 
+
+// Read the specific ID or div on the website
+const element = document.getElementsByClassName('cart-item-name');
+const data = {
+  req: [element.innerText, "Item 2"]
+};
+console.log(element.innerText)
+console.log(data)
+
+// Send a message to the background script
+sendMessageToBackgroundScript(data);
