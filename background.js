bf031905a9153d03ab74f6b749c8d91de95efe03
root <root@MezTop.localdomain>
2023-05-16 18:16:50 +1000

    API working and returning console log

--- background.js
+++ background.js
@@ -13,6 +13,7 @@ function sendHttpRequest(data) {
       .then(response => response.json())
       .then(responseData => {
         // Send the response back to the content script
+        console.log(responseData)
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
           chrome.tabs.sendMessage(tabs[0].id, responseData);
         });
--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 // fetch('http://envirocost.australiaeast.cloudapp.azure.com/structured', {
 
 
+// Function to log the received message
+function logMessage(message) {
+  console.log('Received message:', message);
+}
 
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'fetchData') {
-    // Call the function to fetch data and send it to the API
-    fetchDataAndSendToAPI();
-  }
-});
+chrome.runtime.onMessage.addListener(logMessage);
+
 
 console.log("EnviroCost Ext Loaded")
 
@@ -16,10 +16,10 @@ console.log("EnviroCost Ext Loaded")
 // Read the specific ID or div on the website
 const element = document.getElementsByClassName('cart-item-name');
 const data = {
-  req: [element.innerText, "Item 2"]
+  req: ["element.innerText", "Item 2"]
 };
 console.log(element.innerText)
 console.log(data)
 
 // Send a message to the background script
-sendMessageToBackgroundScript(data);
+chrome.runtime.sendMessage(data);
